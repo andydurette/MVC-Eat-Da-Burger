4c3ce56d0f4da4b95bfb6b9edb9e12e7a11d6e26
Andy Durette <duretteandy@gmail.com>
2021-02-05 18:31:41 -0500

    Repositioned data

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,13 @@
-/*
-DB: heroku_27dc7e1957ed417
-H: us-cdbr-iron-east-05.cleardb.net
-U: baff9b29011800
-P: eadc8d19
-*/
-
 const mysql = require('mysql');
+require("dotenv").config(); 
 
 // Create Connection
 const db_config = {
-  host: 'nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: 'jistvo33ylqfn41m',
-  password: 'd7dffzj4q5nz8kqq',
-  database: 'daj99ebjlhgapx9n',
-  port:3306
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+  port:3306,
+  database: 'daj99ebjlhgapx9n'
 };
 
 module.exports = db_config;
